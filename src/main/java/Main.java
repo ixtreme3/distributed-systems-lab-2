d655ab9361f85cd041269d784630e30f35424c0d
Ivan <ixtreme3@yandex.ru>
2022-04-23 22:44:29 +0700

    Minor changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,23 +22,7 @@ public class Main {
         TagDao tagDao = new TagDaoImpl();
         NodeService nodeService = new NodeService(nodeDao, tagDao);
         OsmProcessor osmProcessor = new OsmProcessor(nodeService);
-        Unpacker.bz2ToOsm();
         osmProcessor.processOsm();
         logger.info("finish");
     }
 }
-/*
-Current input objects: 30000
-Speed: 1875.2344043005376
-Current time: 15998
------------------------------------
-Strategy: PreparedStatement
-Current input objects: 30000public
-Speed: 1867.1811788137177
-Current time: 16067
------------------------------------
-Strategy: Buffered
-Current input objects: 30000
-Speed: 28735.632183908045
-Current time: 1044
- */
